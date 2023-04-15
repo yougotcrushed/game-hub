2a0637c32640f6bb18b2ffb7399dc28b7db32315
Yougotcrushed <gettycrushed@gmail.com>
2023-04-15 12:18:09 +0100

    build Navbar and Dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -11,15 +12,15 @@ function App() {
         lg: `"nav nav" "aside main"`,
       }}
     >
-      <GridItem area="nav" bg={"coral"}>
-        Nav
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" bg={"gold"}>
           Aside
         </GridItem>
       </Show>
-      <GridItem area="main" bg={"doggerblue"}>
+      <GridItem area="main" bg={"dodgerblue"}>
         Main
       </GridItem>
     </Grid>
