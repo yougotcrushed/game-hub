81d9c8c1f7ab001939c96efd3074869747a64816
Yougotcrushed <gettycrushed@gmail.com>
2023-04-18 09:45:10 +0100

    Displaying the genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,11 +11,7 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3 }}
-        padding={"2.5"}
-        spacing={10}
-      >
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding={"2.5"} spacing={3}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer>
