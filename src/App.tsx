de19c119469b45e471e6f4dc385c578fefe47fb9
Yougotcrushed <gettycrushed@gmail.com>
2023-04-20 18:19:02 +0100

    sort the games

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,12 @@ import SortSelector from "./components/SortSelector";
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
+  sortOrder: string;
 }
 
 function App() {
   // const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
-  // const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
-  //   null
-  // );
+  // const [selectedOrder, setSelectedOrder] = useState("");
 
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
 
@@ -53,7 +52,12 @@ function App() {
               setGameQuery({ ...gameQuery, platform })
             }
           />
-          <SortSelector />
+          <SortSelector
+            sortOrder={gameQuery.sortOrder}
+            onSelectSortOrder={(sortOrder) =>
+              setGameQuery({ ...gameQuery, sortOrder })
+            }
+          />
         </HStack>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
