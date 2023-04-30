3f3b0cf27d2289af73e44235441542b7ea707a64
Yougotcrushed <gettycrushed@gmail.com>
2023-04-30 19:11:01 +0100

    Implement infinite scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -53,6 +53,7 @@ const useGames = (gameQuery: GameQuery) =>
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
+    staleTime: 24 * 60 * 60 * 1000, //24h
   });
 
 // const [games, setGames] = useState<Game[]>([]);
