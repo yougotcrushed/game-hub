3cc25d7ec27bf55f34f5bc29e472a4e5d1ed68f4
Yougotcrushed <gettycrushed@gmail.com>
2023-05-16 09:16:50 +0100

    Handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/layout";
 import Homepage from "./pages/Homepage";
 import GameDetailPage from "./pages/GameDetailPage";
+import ErrorsPage from "./pages/ErrorsPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorsPage />,
     children: [
       { index: true, element: <Homepage /> },
       { path: "game/:id", element: <GameDetailPage /> },
