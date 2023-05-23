59f1718d212b85aeefb6257be949a819d3f19bd2
Yougotcrushed <gettycrushed@gmail.com>
2023-05-23 15:00:25 +0100

    Build game screenshots

--- src/components/GameScreenshot.tsx
+++ src/components/GameScreenshot.tsx
@@ -13,7 +13,7 @@ const GameScreenshot = ({ gameId }: Props) => {
   if (error) throw error;
   console.log(data);
   return (
-    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={2}>
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={1.5}>
       {data?.results.map((file) => (
         <Image key={file.id} src={file.image} />
       ))}
